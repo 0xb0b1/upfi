32bbb4926b20cf2c738bd8438d78dce420f804f0
0xb0b1 <vcente82@gmail.com>
2021-06-08 10:04:43 -0300

    View Image Modal basic feature

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,31 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal onClose={onClose} isOpen={isOpen}>
+      <ModalOverlay bg="rgba(0, 0, 0, 0.3)" />
+      <ModalContent
+        my="auto"
+        alignItems="start"
+        maxWidth="900px"
+        maxHeight="600px"
+      >
+        <ModalBody p="0">
+          <Image src={imgUrl} alt="image" objectFit="cover" layout="fill" />
+        </ModalBody>
+        <ModalFooter
+          h="2rem"
+          w="100%"
+          p="2"
+          color="pGray.50"
+          bg="pGray.800"
+          justifyContent="left"
+        >
+          <Link fontSize="0.87rem" href={imgUrl}>
+            Ver original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
