4d67dff50f5552fe84ec4584bd8e351b5eab243b
0xb0b1 <vcente82@gmail.com>
2021-06-08 15:03:38 -0300

    ...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable no-console */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Button, Box } from '@chakra-ui/react';
 import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
@@ -9,6 +13,8 @@ import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
 export default function Home(): JSX.Element {
+  const fetchImages = ({ pageParam = 0 }) => api.get('/api/images');
+
   const {
     data,
     isLoading,
@@ -16,13 +22,9 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    null,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-    null
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: (lastPage, pages) => lastPage,
+  });
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
@@ -32,13 +34,34 @@ export default function Home(): JSX.Element {
 
   // TODO RENDER ERROR SCREEN
 
+  // if (isLoading) {
+  //   return (
+  //     <>
+  //       <Loading />
+  //     </>
+  //   );
+  // }
+
+  // if (isError) {
+  //   return (
+  //     <>
+  //       <Error />
+  //     </>
+  //   );
+  // }
+
   return (
     <>
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
+
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        <Button mt={8} onClick={() => console.log('')}>
+          Carregar mais
+        </Button>
       </Box>
     </>
   );
